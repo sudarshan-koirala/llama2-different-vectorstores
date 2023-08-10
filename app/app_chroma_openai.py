9a2cb55953723c247fde2c76dd0a4a4f1df57f69
Sudarshan Koirala <snkoirala45@gmail.com>
2023-08-10 10:49:39 +0000

    code for running the chainlit app

---
+++ app/app_chroma_openai.py
@@ -0,0 +1,176 @@
+import os
+
+import chainlit as cl
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import HuggingFaceEmbeddings, OpenAIEmbeddings
+from langchain.llms import CTransformers
+from langchain.prompts import PromptTemplate
+from langchain.vectorstores import Chroma
+
+DB_FAISS_PATH = "./../vectorstore/db_chroma_openai"
+
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+
+prompt_template = """Use the following pieces of context to answer the users question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+ALWAYS return a "SOURCES" part in your answer.
+The "SOURCES" part should be a reference to the source of the document from which you got your answer.
+Example of your response should be as follows:
+
+Context: {context}
+Question: {question}
+
+Only return the helpful answer below and nothing else.
+Helpful answer:
+"""
+
+
+def set_custom_prompt():
+    """
+    Prompt template for QA retrieval for each vectorstore
+    """
+    prompt = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
+    return prompt
+
+
+def create_retrieval_qa_chain(llm, prompt, db):
+    """
+    Creates a Retrieval Question-Answering (QA) chain using a given language model, prompt, and database.
+
+    This function initializes a RetrievalQA object with a specific chain type and configurations,
+    and returns this QA chain. The retriever is set up to return the top 3 results (k=3).
+
+    Args:
+        llm (any): The language model to be used in the RetrievalQA.
+        prompt (str): The prompt to be used in the chain type.
+        db (any): The database to be used as the retriever.
+
+    Returns:
+        RetrievalQA: The initialized QA chain.
+    """
+    qa_chain = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=db.as_retriever(search_kwargs={"k": 3}),
+        return_source_documents=True,
+        chain_type_kwargs={"prompt": prompt},
+    )
+    return qa_chain
+
+
+# initialize openai chat model
+llm = ChatOpenAI(model="gpt-3.5-turbo")
+
+
+def create_retrieval_qa_bot(
+    model_name="text-embedding-ada-002", persist_dir=DB_FAISS_PATH
+):
+    """
+    This function creates a retrieval-based question-answering bot.
+
+    Parameters:
+        model (str): The name of the model to be used for embeddings.
+        persist_dir (str): The directory to persist the database.
+
+    Returns:
+        RetrievalQA: The retrieval-based question-answering bot.
+
+    """
+
+    if not os.path.exists(persist_dir):
+        raise FileNotFoundError(f"No directory found at {persist_dir}")
+
+    try:
+        embeddings = OpenAIEmbeddings(model=model_name)  # type: ignore
+    except Exception as e:
+        raise Exception(
+            f"Failed to load embeddings with model name {model_name}: {str(e)}"
+        )
+
+    db = Chroma(persist_directory=persist_dir, embedding_function=embeddings)
+
+    qa_prompt = (
+        set_custom_prompt()
+    )  # Assuming this function exists and works as expected
+
+    try:
+        qa = create_retrieval_qa_chain(
+            llm=llm, prompt=qa_prompt, db=db
+        )  # Assuming this function exists and works as expected
+    except Exception as e:
+        raise Exception(f"Failed to create retrieval QA chain: {str(e)}")
+
+    return qa
+
+
+def retrieve_bot_answer(query):
+    """
+    Retrieves the answer to a given query using a QA bot.
+
+    This function creates an instance of a QA bot, passes the query to it,
+    and returns the bot's response.
+
+    Args:
+        query (str): The question to be answered by the QA bot.
+
+    Returns:
+        dict: The QA bot's response, typically a dictionary with response details.
+    """
+    qa_bot_instance = create_retrieval_qa_bot()
+    bot_response = qa_bot_instance({"query": query})
+    return bot_response
+
+
+@cl.on_chat_start
+async def initialize_bot():
+    """
+    Initializes the bot when a new chat starts.
+
+    This asynchronous function creates a new instance of the retrieval QA bot,
+    sends a welcome message, and stores the bot instance in the user's session.
+    """
+    qa_chain = create_retrieval_qa_bot()
+    welcome_message = cl.Message(content="Starting the bot...")
+    await welcome_message.send()
+    welcome_message.content = (
+        "Hi, Welcome to Chat With Documents using Llama2 and LangChain."
+    )
+    await welcome_message.update()
+
+    cl.user_session.set("chain", qa_chain)
+
+
+@cl.on_message
+async def process_chat_message(message):
+    """
+    Processes incoming chat messages.
+
+    This asynchronous function retrieves the QA bot instance from the user's session,
+    sets up a callback handler for the bot's response, and executes the bot's
+    call method with the given message and callback. The bot's answer and source
+    documents are then extracted from the response.
+    """
+    qa_chain = cl.user_session.get("chain")
+    callback_handler = cl.AsyncLangchainCallbackHandler(
+        stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
+    )
+    callback_handler.answer_reached = True
+    response = await qa_chain.acall(message, callbacks=[callback_handler])
+    bot_answer = response["result"]
+    source_documents = response["source_documents"]
+
+    if source_documents:
+        bot_answer += f"\nSources:" + str(source_documents)
+    else:
+        bot_answer += "\nNo sources found"
+
+    await cl.Message(content=bot_answer).send()
